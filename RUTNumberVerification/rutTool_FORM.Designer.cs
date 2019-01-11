ddd9fd4f0ae051e5aea5a6a8785847e903ef76da
Eddy Borremans <eddy.borremans@emagine-reality.com>
2019-01-11 09:53:26 +0100

    First version

---
+++ RUTNumberVerification/rutTool_FORM.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RUTNumberVerification
+{
+    partial class rutTool_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rutNumber_TXT = new System.Windows.Forms.TextBox();
+            this.isValidRUT = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.generateRUT_BTN = new System.Windows.Forms.Button();
+            this.test_BTN = new System.Windows.Forms.Button();
+            this.nrOfTests_UPDWN = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nrOfTests_UPDWN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rutNumber_TXT
+            // 
+            this.rutNumber_TXT.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rutNumber_TXT.Location = new System.Drawing.Point(14, 136);
+            this.rutNumber_TXT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rutNumber_TXT.Name = "rutNumber_TXT";
+            this.rutNumber_TXT.Size = new System.Drawing.Size(216, 49);
+            this.rutNumber_TXT.TabIndex = 0;
+            this.rutNumber_TXT.Text = "13460882-k";
+            this.rutNumber_TXT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.rutNumber_TXT.TextChanged += new System.EventHandler(this.rutNumber_TXT_TextChanged);
+            // 
+            // isValidRUT
+            // 
+            this.isValidRUT.AutoSize = true;
+            this.isValidRUT.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.isValidRUT.Location = new System.Drawing.Point(249, 140);
+            this.isValidRUT.Name = "isValidRUT";
+            this.isValidRUT.Size = new System.Drawing.Size(228, 42);
+            this.isValidRUT.TabIndex = 1;
+            this.isValidRUT.Text = "<not verified>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter RUT (inclusing verification character)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "<7 or 8 digits>-<1 digit or \'K\'> :";
+            // 
+            // generateRUT_BTN
+            // 
+            this.generateRUT_BTN.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateRUT_BTN.Location = new System.Drawing.Point(569, 131);
+            this.generateRUT_BTN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.generateRUT_BTN.Name = "generateRUT_BTN";
+            this.generateRUT_BTN.Size = new System.Drawing.Size(257, 54);
+            this.generateRUT_BTN.TabIndex = 4;
+            this.generateRUT_BTN.Text = "Generate RUT";
+            this.generateRUT_BTN.UseVisualStyleBackColor = true;
+            this.generateRUT_BTN.Click += new System.EventHandler(this.generateRUT_BTN_Click);
+            // 
+            // test_BTN
+            // 
+            this.test_BTN.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test_BTN.Location = new System.Drawing.Point(569, 51);
+            this.test_BTN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.test_BTN.Name = "test_BTN";
+            this.test_BTN.Size = new System.Drawing.Size(87, 54);
+            this.test_BTN.TabIndex = 5;
+            this.test_BTN.Text = "Test";
+            this.test_BTN.UseVisualStyleBackColor = true;
+            this.test_BTN.Click += new System.EventHandler(this.test_BTN_Click);
+            // 
+            // nrOfTests_UPDWN
+            // 
+            this.nrOfTests_UPDWN.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrOfTests_UPDWN.Increment = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nrOfTests_UPDWN.Location = new System.Drawing.Point(662, 54);
+            this.nrOfTests_UPDWN.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nrOfTests_UPDWN.Name = "nrOfTests_UPDWN";
+            this.nrOfTests_UPDWN.Size = new System.Drawing.Size(164, 48);
+            this.nrOfTests_UPDWN.TabIndex = 7;
+            this.nrOfTests_UPDWN.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nrOfTests_UPDWN.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // rutTool_FORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 204);
+            this.Controls.Add(this.nrOfTests_UPDWN);
+            this.Controls.Add(this.test_BTN);
+            this.Controls.Add(this.generateRUT_BTN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isValidRUT);
+            this.Controls.Add(this.rutNumber_TXT);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "rutTool_FORM";
+            this.Text = "RUT Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.nrOfTests_UPDWN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rutNumber_TXT;
+        private System.Windows.Forms.Label isValidRUT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button generateRUT_BTN;
+        private System.Windows.Forms.Button test_BTN;
+        private System.Windows.Forms.NumericUpDown nrOfTests_UPDWN;
+    }
+}
+
---
+++ RUTNumberVerification/rutTool_FORM.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace RUTNumberVerification
+{
+    public partial class rutTool_FORM : Form
+    {
+        private Boolean beingChanged = false;
+        private String RUT_SEPARATORS = "\\.|\\,|\\-";
+
+        public enum RUTResult
+        {
+            VALID, BAD_FORMAT, BAD_CHECKSUM, STRING_TOO_LONG, STRING_TOO_SHORT
+        }
+
+        public rutTool_FORM()
+        {
+            InitializeComponent();
+            CustomInit();
+        }
+
+        private void CustomInit()
+        {
+            checkRUT();
+        }
+
+        private void rutNumber_TXT_TextChanged(object sender, EventArgs e)
+        {
+            checkRUT();
+        }
+
+        private void checkRUT()
+        {
+            RUTResult result = verifyRUT2(rutNumber_TXT.Text);
+            Debug.WriteLine(rutNumber_TXT.Text + " => " + result.ToString());
+            isValidRUT.Text = result.ToString();
+        }
+
+        /// <summary>
+        /// Routine for checking of RUT correctness
+        /// </summary>
+        /// <param name="rut">RUT to check</param>
+        /// <returns>true if RUT is valid</returns>
+        /// <remarks>Only numbers and optional "K" at the end of string are expected
+        /// </remarks>
+        public RUTResult verifyRUT1(string rut)
+        {
+            if (!beingChanged)
+            {
+                beingChanged = true;
+                rutNumber_TXT.Text = rutNumber_TXT.Text.ToUpper();
+                beingChanged = false;
+            }
+
+            Debug.WriteLine("Input RUT               : " + rut);
+            // rut = rut.Replace("-", "");
+            rut = Regex.Replace(rut, @",|-|\.", "");
+            // Debug.WriteLine("RUT after stripping '-' : " + rut);
+
+            const string RutRegex = "[0-9]+K?";
+            Regex RegExRut = new Regex(RutRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            int[] coefs = { 3, 2, 7, 6, 5, 4, 3, 2 };
+
+            // In case that rut is padded with spaces
+            rut = rut.Trim().ToUpperInvariant();
+
+            if (!RegExRut.IsMatch(rut)) { return RUTResult.BAD_FORMAT; }
+
+            if (rut.Length > 9) { return RUTResult.STRING_TOO_LONG; }
+
+            if (rut.Length < 8) { return RUTResult.STRING_TOO_SHORT; }
+
+            // If shorter than 9 characters (8 + control char) ...
+            rut = rut.PadLeft(9, '0');
+
+            int total = 0;
+
+            for (int index = 0; index < rut.Length - 1; index++)
+            {
+                char curr = rut.Substring(index, 1).ToCharArray()[0];
+                total += coefs[index] * (curr - '0');
+            }
+
+            int rest = 11 - (total % 11);
+
+            if (rest == 11) rest = 0;
+
+            if ((rest == 10) && rut.EndsWith("K")) { return RUTResult.VALID; }
+
+            // if (rut.Substring(rut.Length - 1, 1).ToCharArray()[0] == ('0' + rest))
+            if(rut.Substring(rut.Length - 1, 1) == rest.ToString())
+            {
+                return RUTResult.VALID;
+            }
+
+            return RUTResult.BAD_CHECKSUM;
+        }
+
+        /// <summary>
+        /// Alternative routine for checking of RUT correctness
+        /// </summary>
+        /// <param name="rut">RUT to check</param>
+        /// <returns>true if RUT is valid</returns>
+        /// <remarks>Only numbers and optional "K" at the end of string are expected
+        /// </remarks>
+        public RUTResult verifyRUT2(string rut)
+        {
+            if (String.IsNullOrEmpty(rut))
+            {
+                return RUTResult.BAD_CHECKSUM;
+            }
+            String rutUpperCase =  rut.ToUpper();
+
+            Regex rgx = new Regex(RUT_SEPARATORS);
+            String rutAlphaNum = rgx.Replace(rutUpperCase, "");
+
+            char checkSum = rutAlphaNum[rutAlphaNum.Length - 1];
+
+            String rutNbrStr = rutAlphaNum.Substring(0, rutAlphaNum.Length - 1);
+
+            try {
+              int rutNbr = Int32.Parse(rutNbrStr);
+                int itr = 0, sum = 1;
+
+                for (; rutNbr != 0; rutNbr /= 10) {
+                    sum = (sum + rutNbr % 10 * (9 - itr % 6)) % 11;
+                    itr++;
+                }
+                char calCheckSum = (char) (sum != 0 ? sum + 47 : 75);
+                if (checkSum != calCheckSum) {
+                    return RUTResult.BAD_CHECKSUM;
+                } else {
+                    return RUTResult.VALID;
+                }
+            } catch (FormatException e) {
+                return RUTResult.BAD_FORMAT;
+            }
+            catch (Exception e) {
+                return RUTResult.BAD_CHECKSUM;
+            }
+
+            return RUTResult.BAD_CHECKSUM;
+        }
+
+        private void generateRUT_BTN_Click(object sender, EventArgs e)
+        {
+            rutNumber_TXT.Text = generateRUT();
+        }
+
+        private String generateRUT()
+        {
+            String RUTDigitPart = generate7or8DigitNumberString();
+            String validRUT = addRUTVerificationPart(RUTDigitPart);
+
+            return validRUT;
+        }
+
+        private String generate7or8DigitNumberString()
+        {
+            int seed = DateTime.Now.Millisecond;
+            Random r = new Random(seed);
+            Int32 threeOrFourDigitInt = r.Next(100, 9999);
+            Int32 fourDigitInt = r.Next(1000, 9999);
+
+            return threeOrFourDigitInt.ToString() + fourDigitInt.ToString();
+        }
+
+        private String addRUTVerificationPart(String RUTDigitPart)
+        {
+            String[] verificationCharacters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "K" };
+            int verificationCharIndex = 0;
+            String RUTWithChecksum = "";
+            RUTResult result1 = RUTResult.BAD_CHECKSUM;
+            RUTResult result2 = RUTResult.BAD_CHECKSUM;
+
+            do
+            {
+                RUTWithChecksum = RUTDigitPart + "-" + verificationCharacters[verificationCharIndex];
+                result1 = verifyRUT1(RUTWithChecksum);
+                result2 = verifyRUT2(RUTWithChecksum);
+                if (result1 != RUTResult.VALID)
+                {
+                    Debug.WriteLine(result1 + " retrying...");
+                }
+                if (result2 != RUTResult.VALID)
+                {
+                    Debug.WriteLine(result2 + " retrying...");
+                }
+                verificationCharIndex++;
+            } while ((result1 != RUTResult.VALID && verificationCharIndex < verificationCharacters.Length) || result1 != result2);
+
+            if (result1 != result2)
+            {
+                throw new Exception("Inconsistent RUT verification results (" + result1 + " vs " + result2 + ") for '" + RUTDigitPart + "'");
+            }
+            else
+            if (result1 != RUTResult.VALID)
+            {
+                throw new Exception("Couldn't generate verification digit for '" + RUTDigitPart + "'");
+            }
+            else
+            {
+                Debug.WriteLine("Found valid RUT: " + RUTWithChecksum);
+            }
+
+            return RUTWithChecksum;
+        }
+
+        private void test_BTN_Click(object sender, EventArgs e)
+        {
+            String rut = "";
+            Cursor currentCursor = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+            for (int i = 0; i < nrOfTests_UPDWN.Value; i++)
+            {
+                rut = generateRUT();
+            }
+            Cursor.Current = currentCursor;
+            rutNumber_TXT.Text = rut;
+        }
+    }
+}
